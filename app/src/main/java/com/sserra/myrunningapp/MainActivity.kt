7ef0b73cc3a8db7d19713b003f2fd95442ef2c13
stevie990 <57816129+stevie990@users.noreply.github.com>
2020-06-11 22:53:12 +0200

    Initial commit

---
+++ app/src/main/java/com/sserra/myrunningapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sserra.myrunningapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
