94dd7a741bdc75866cbf33f1adf93c3e8f2b9447
stevie990 <57816129+stevie990@users.noreply.github.com>
2020-06-23 16:27:09 +0200

    Set up navigation
    Set up navigation using navigation component

--- app/src/main/java/com/sserra/myrunningapp/ui/MainActivity.kt
+++ app/src/main/java/com/sserra/myrunningapp/ui/MainActivity.kt
@@ -2,21 +2,30 @@ package com.sserra.myrunningapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sserra.myrunningapp.R
-import com.sserra.myrunningapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDAO
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("runDao", "RunDAO: ${runDAO.hashCode()}")
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
