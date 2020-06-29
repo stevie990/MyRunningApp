842dafc429c63a2d9a330cb54947b834c3376f8d
stevie990 <57816129+stevie990@users.noreply.github.com>
2020-06-29 17:34:51 +0200

    Tracking Service setup

--- app/src/main/java/com/sserra/myrunningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/sserra/myrunningapp/ui/fragments/TrackingFragment.kt
@@ -1,11 +1,14 @@
 package com.sserra.myrunningapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
 import com.sserra.myrunningapp.R
+import com.sserra.myrunningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.sserra.myrunningapp.services.TrackingService
 import com.sserra.myrunningapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
@@ -19,13 +22,22 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         mapView?.onCreate(savedInstanceState)
+
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
 
         mapView.getMapAsync {
             map = it
         }
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
 
     override fun onResume() {
         super.onResume()
