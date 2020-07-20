91e11543657b79de09ceefb894595f6c5b9495ae
stevie990 <57816129+stevie990@users.noreply.github.com>
2020-07-20 19:03:43 +0200

    Sort runs using a MediatorLiveData

--- app/src/main/java/com/sserra/myrunningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/sserra/myrunningapp/ui/fragments/RunFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.sserra.myrunningapp.R
 import com.sserra.myrunningapp.adapters.RunAdapter
 import com.sserra.myrunningapp.other.Constants.REQUEST_CODE_LOCATION_PERMISSIONS
+import com.sserra.myrunningapp.other.SortType
 import com.sserra.myrunningapp.other.TrackingUtility
 import com.sserra.myrunningapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,7 +34,29 @@ class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionC
         requestPermissions()
         setupRecyclerView()
 
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+        when(viewModel.sortType) {
+            SortType.DATE -> spinnerFilter.setSelection(0)
+            SortType.RUNNING_TIME -> spinnerFilter.setSelection(1)
+            SortType.DISTANCE -> spinnerFilter.setSelection(2)
+            SortType.AVG_SPEED -> spinnerFilter.setSelection(3)
+            SortType.CALORIES_BURNED -> spinnerFilter.setSelection(4)
+        }
+
+        spinnerFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when(position){
+                    0 -> viewModel.sortRuns(SortType.DATE)
+                    1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRuns(SortType.DISTANCE)
+                    3 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    4 -> viewModel.sortRuns(SortType.CALORIES_BURNED)
+                }
+            }
+        }
+
+        viewModel.runs.observe(viewLifecycleOwner, Observer {
             runAdapter.submitList(it)
         })
 
--- app/src/main/java/com/sserra/myrunningapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/sserra/myrunningapp/ui/viewmodels/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.sserra.myrunningapp.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sserra.myrunningapp.db.Run
+import com.sserra.myrunningapp.other.SortType
 import com.sserra.myrunningapp.repositories.MainRepository
 import kotlinx.coroutines.launch
 
@@ -11,7 +13,57 @@ class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
 
-    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+    private val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+    private val runsSortedByDistance = mainRepository.getAllRunsSortedByDistance()
+    private val runsSortedByCalories = mainRepository.getAllRunsSortedByCalories()
+    private val runsSortedByTimeInMilliseconds = mainRepository.getAllRunsSortedByTimeInMillis()
+    private val runsSortedByAvgSpeed = mainRepository.getAllRunsSortedByAvgSpeed()
+
+    val runs = MediatorLiveData<List<Run>>()
+
+    var sortType = SortType.DATE
+
+    init {
+        runs.addSource(runsSortedByDate) { result ->
+            if (sortType == SortType.DATE) {
+                result?.let { runs.value = it }
+            }
+        }
+
+        runs.addSource(runsSortedByAvgSpeed) { result ->
+            if (sortType == SortType.AVG_SPEED) {
+                result?.let { runs.value = it }
+            }
+        }
+
+        runs.addSource(runsSortedByCalories) { result ->
+            if (sortType == SortType.CALORIES_BURNED) {
+                result?.let { runs.value = it }
+            }
+        }
+
+        runs.addSource(runsSortedByDistance) { result ->
+            if (sortType == SortType.DISTANCE) {
+                result?.let { runs.value = it }
+            }
+        }
+
+        runs.addSource(runsSortedByTimeInMilliseconds) { result ->
+            if (sortType == SortType.RUNNING_TIME) {
+                result?.let { runs.value = it }
+            }
+        }
+    }
+
+    fun sortRuns(sortType: SortType) = when(sortType) {
+        SortType.DATE -> runsSortedByDate.value?.let { runs.value = it }
+        SortType.DISTANCE -> runsSortedByDistance.value?.let { runs.value = it }
+        SortType.CALORIES_BURNED -> runsSortedByCalories.value?.let { runs.value = it }
+        SortType.RUNNING_TIME -> runsSortedByTimeInMilliseconds.value?.let { runs.value = it }
+        SortType.AVG_SPEED -> runsSortedByAvgSpeed.value?.let { runs.value = it }
+    }.also {
+        this.sortType = sortType
+    }
 
     fun insertRun(run: Run) = viewModelScope.launch {
         mainRepository.insertRun(run)
